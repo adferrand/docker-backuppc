d65d619fff15eca68babb44aab8fa6badb300824
Konstantin Dmitriev <ksee.zelgadis@gmail.com>
2019-01-29 12:13:32 +0700

    Workaround bug which causes BackupPC fail to start when container launched with existing configuration

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -34,6 +34,9 @@ if [ -f /firstrun ]; then
 	# Configure WEB UI access
 	sed -ie "s/^\$Conf{CgiAdminUsers}\s*=\s*'\w*'/\$Conf{CgiAdminUsers} = '${BACKUPPC_WEB_USER:-backuppc}'/g" /etc/backuppc/config.pl
 	htpasswd -b -c /etc/backuppc/htpasswd ${BACKUPPC_WEB_USER:-backuppc} ${BACKUPPC_WEB_PASSWD:-password}
+	
+	# Workaround bug which causes BackupPC fail to start when container launched with existing configuration
+	sed -ie "s|^\$Conf{CgiURL}\s*=\s*''http://localhost/cgi-bin/BackupPC/BackupPC_Admin''|\$Conf{CgiURL} = 'http://localhost/cgi-bin/BackupPC/BackupPC_Admin'|g" /etc/backuppc/config.pl
 
 	# Prepare lighttpd
 	if [ "$USE_SSL" = true ]; then
