cd25cd329da6ae09fb0e9fd3cba27fa663d68e5c
Adrien Ferrand <ferrand.ad@gmail.com>
2019-03-10 20:58:06 +0100

    Use a minimal passphrase of 4 characters to be accepted by newest versions of openssl

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -72,8 +72,8 @@ if [ -f /firstrun ]; then
 		if [ ! -e /etc/lighttpd/server.pem ]; then
 			# Generate certificate file as needed
 			cd /etc/lighttpd
-			openssl genrsa -des3 -passout pass:x -out server.pass.key 2048
-			openssl rsa -passin pass:x -in server.pass.key -out server.key
+			openssl genrsa -des3 -passout pass:1234 -out server.pass.key 2048
+			openssl rsa -passin pass:1234 -in server.pass.key -out server.key
 			openssl req -new -key server.key -out server.csr \
 				-subj "/C=UK/ST=Warwickshire/L=Leamington/O=OrgName/OU=IT Department/CN=example.com"
 			openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
